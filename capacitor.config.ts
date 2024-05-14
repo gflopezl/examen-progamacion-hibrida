4dad0a3faff781381ba2f268d9c928a8b71140da
gflopezl <gflopezl@alumno.iplacex.cl>
2024-05-14 17:45:06 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'examen.progamacion.hibrida',
+  appName: 'examen-progamacion-hibrida',
+  webDir: 'www'
+};
+
+export default config;
